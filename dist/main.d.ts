2baca4a8cac7dccbe4df8a5ef93ef5e57cd62009
EduSantosBrito <edu.santos.brito@gmail.com>
2026-04-22 00:08:43 +0200

    Remove dist from git

--- dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-import { Layer } from "effect";
-export declare const HttpServerLayer: Layer.Layer<never, import("effect/unstable/http/HttpServerError").ServeError, never>;
-//# sourceMappingURL=main.d.ts.map
